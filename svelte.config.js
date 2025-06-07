133fff65c34eb89e4441a6f7504ab549912eae0e
YOSHIOKA Eiichiro <yoshioka@ossia.co.jp>
2025-06-07 17:10:48 +0900

    init

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+	
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			out: 'build',
+			precompress: false,
+			envPrefix: ''
+		})
 	}
 };
 
